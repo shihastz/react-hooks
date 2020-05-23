9d476c3bcd68ec966fef5fe027f779515a8cbddf
shihastz <shihast0503@gmail.com>
2020-05-23 16:56:22 +0530

    changes

--- src/componenet/hooks/EffectHookCounter.js
+++ src/componenet/hooks/EffectHookCounter.js
@@ -1,25 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 function EffectHooksCounter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
-    const[count, setCount] = useState(0)
-    const[name, setName] = useState('')
+  useEffect(() => {
+    console.log("title changes");
+    document.title = `You clicked ${count} times`;
+  }, [count]);
 
-    useEffect(() => {
-        console.log('title changes')
-        document.title = `You clicked ${count} times`
-    }, [count])
-
-
-    return (
-        <div>
-            <hr/>
-            <h2>Counter in website title using useEffect (useEffect only run when count state changes)</h2> 
-            <input type="text" value={name} onChange={e => setName(e.target.value)} />
-            <button onClick={() => setCount(count + 1)}>Count {count}</button>
-            <hr/>
-        </div>
-    )
+  return (
+    <div>
+      <hr />
+      <h2>
+        Counter in website title using useEffect (useEffect only run when count
+        state changes)
+      </h2>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
+      <hr />
+    </div>
+  );
 }
 
-export default EffectHooksCounter
+export default EffectHooksCounter;
--- src/componenet/hooks/customHooks/CustomUseInput.js
+++ src/componenet/hooks/customHooks/CustomUseInput.js
@@ -5,28 +5,30 @@ function CustomUseInput() {
   const [firstName, bindFirstname, resetFirstname] = useInput("");
   const [lastName, bindLastname, resetLastname] = useInput("");
 
-  const submitHandler = e => {
+  const submitHandler = (e) => {
     e.preventDefault();
     alert(`hi ${firstName} ${lastName}`);
 
-    resetFirstname()
-    resetLastname()
+    resetFirstname();
+    resetLastname();
   };
   return (
     <div>
-        <hr/>
-        <h3>Custom Form using useInput custom hook</h3>
+      <hr />
+      <h3>Custom Form using useInput custom hook</h3>
       <form onSubmit={submitHandler}>
         <div>
-            <label>First Name</label>
-            <input type="text" {...bindFirstname}/>
+          <label>First Name</label>
+          <input type="text" {...bindFirstname} />
         </div>
         <div>
-            <label>Last Name</label>
-            <input type="text" {...bindLastname}/>
+          <label>Last Name</label>
+          <input type="text" {...bindLastname} />
         </div>
         <div>
-            <button type="button" onClick={submitHandler}>Submit</button>
+          <button type="button" onClick={submitHandler}>
+            Submit
+          </button>
         </div>
       </form>
     </div>
